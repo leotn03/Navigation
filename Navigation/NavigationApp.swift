0256d082b0644f3c92bccb8f815587f944febbcd
leotn03 <leo.tn03@icloud.com>
2024-01-17 10:57:50 -0500

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Leo Torres Neyra on 17/1/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
